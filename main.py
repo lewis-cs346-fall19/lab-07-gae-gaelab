ba3e9157704482c4ed69b752aa4244a52398209c
EC2 Default User <ec2-user@ip-172-31-39-213.ec2.internal>
2019-11-16 01:55:20 +0000

    Connection sorta working

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import webapp2
+import webapp2, MySQLdb, passwords
+
+
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers["Content-Type"] = "text/html"
-		self.response.write("Hello world")
+
+                conn = MySQLdb.connect(unix_socket = passwords.SQL_HOST, user = passwords.SQL_USER, passwd = passwords.SQL_PASSWD,db = 'testTable')
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM testTable");
+                results = cursor.fetchall()
+                cursor.close()
+                print(results)
+                self.response.write("gahaaahahahahah")
 
 app = webapp2.WSGIApplication([("/", MainPage),], debug=True)
---
+++ passwords.py
@@ -0,0 +1,3 @@
+SQL_HOST = "/cloudsql/csc346gae:us-central1:csc346gae2"
+SQL_USER = "root"
+SQL_PASSWD = "aceman25"
