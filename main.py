4654b626b6c0e143bb242db52d9ca9e031048de6
matte <mcheffer@email.arizona.edu>
2019-11-16 12:24:41 -0700

    experimenting with cookies

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 import webapp2, MySQLdb, passwords
 from random import getrandbits
 
+create_user_name = "<form method='get'>Create User Name: <input type='text' name='user_name'><input type='submit' value='Create'/></form>"
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         
         self.response.headers["Content-Type"] = "text/html"
 
-        cookie = self.request.cookies.get("cookie_name")
-
-        conn = MySQLdb.connect(unix_socket = passwords.SQL_HOST, user = passwords.SQL_USER, passwd = passwords.SQL_PASSWD,db = 'lab7')
-        cursor = conn.cursor()
+        cookie = self.request.cookies.get("csc346gae")
 
         if cookie:   
             self.response.write(cookie) 
             cursor.execute("SELECT user_name FROM sessions WHERE session_id=%s;",(cookie,))
+            results = cursor.fetchall()
+            self.response.write("Hello " + str(results))    
                 
         else:
-            self.response.write("<form method='post'>Create User Name: <input type='text' name='user_name'><input type='submit' value='Create'/></form>")
+            self.response.write(create_user_name)
+
+        
+
+    def post(self):
+
+        user_name = sefl.request.get("user_name")
+        if user_name == "":
+            self.redirect("/")
+        else:
+            conn = MySQLdb.connect(unix_socket = passwords.SQL_HOST, user = passwords.SQL_USER, passwd = passwords.SQL_PASSWD,db = 'lab7')
+            cursor = conn.cursor()
+
             new_session_id = "%032x" % getrandbits(128)
-            self.response.set_cookie(key='cookie_name', value=new_session_id, max_age=1800,)
+            self.response.set_cookie(key='csc346gae', value=new_session_id, max_age=1800,)
 
             cursor.execute("INSERT INTO sessions (session_id, user_name) VALUES (%s, %s);", (new_session_id, 'user_name'))
             conn.commit()
-            cursor.execute("SELECT user_name FROM sessions WHERE session_id=%s;",(new_session_id,))
-
-        results = cursor.fetchall()
-        self.response.write("Hello " + str(results))    
-        conn.close()
-
+            conn.close()
+            self.redirect("/")
 
 
 app = webapp2.WSGIApplication([("/", MainPage),], debug=True)
