3f71ab97d849d71202ae08017bbd9cc24a6a98f2
matte <mcheffer@email.arizona.edu>
2019-11-16 10:52:52 -0700

    experimenting with cookies

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MainPage(webapp2.RequestHandler):
             cursor.execute("SELECT user_name FROM sessions WHERE session_id=%s;",(new_session_id,))
 
         results = cursor.fetchall()
-        self.response.write("Hello " + str(results[0][0]))    
+        self.response.write("Hello " + str(results))    
         conn.close()
 
 app = webapp2.WSGIApplication([("/", MainPage),], debug=True)
