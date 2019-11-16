d9882b468b54eb1d6abdfc09f3669e47b2d6da65
matte <mcheffer@email.arizona.edu>
2019-11-16 13:36:25 -0700

    experimenting with cookies

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class MainPage(webapp2.RequestHandler):
     
     def get(self):
 
-        increment = "<p><form action=self.redirect('/') method='post'><input type=hidden value=1><input type='submit' value='Increment'/></form>"
+        increment = "<p><form action='/' method='post'><input type=hidden value=1><input type='submit' value='Increment'/></form>"
         
         self.response.headers["Content-Type"] = "text/html"
 
@@ -25,7 +25,7 @@ class MainPage(webapp2.RequestHandler):
         else:
 
             create_user_name = "<p> Welcome, please create a user name to continue<br><form method='post'>Create User Name: <input type='text' name='user_name'><input type='submit' value='Create'/></form>"
-            
+
             self.response.write(create_user_name)
 
         
