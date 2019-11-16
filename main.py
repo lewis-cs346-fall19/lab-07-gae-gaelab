98a111bf4ea86ca705af678eacc7082700460476
matte <mcheffer@email.arizona.edu>
2019-11-15 19:46:44 -0700

    create cookie

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import webapp2, MySQLdb, passwords
 
-
-
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers["Content-Type"] = "text/html"
+		
+        self.response.headers["Content-Type"] = "text/html"
+        conn = MySQLdb.connect(unix_socket = passwords.SQL_HOST, user = passwords.SQL_USER, passwd = passwords.SQL_PASSWD,db = 'lab7')
+            cursor = conn.cursor()
+        cookie = self.request.cookies.get("cookie_name")
 
-                conn = MySQLdb.connect(unix_socket = passwords.SQL_HOST, user = passwords.SQL_USER, passwd = passwords.SQL_PASSWD,db = 'testTable')
-                cursor = conn.cursor()
-                cursor.execute("SELECT * FROM testTable");
-                results = cursor.fetchall()
-                cursor.close()
-                self.response.write(results)
+        if cookie:    
+            q = "SELECT user_name FROM sessions;"
+                
+        else:
+            session_id = "%032x" % random.getrandbits(128)
+            self.response.set_cookie(cookie, session_id, max_age=1800)
+            cursor.execute("INSERT INTO sessions VALUES (session_id, user_name) ")
+
+        cursor.execute(q);
+        results = cursor.fetchall()
+        cursor.close()
+        self.response.write(results[1])    
 
 app = webapp2.WSGIApplication([("/", MainPage),], debug=True)
